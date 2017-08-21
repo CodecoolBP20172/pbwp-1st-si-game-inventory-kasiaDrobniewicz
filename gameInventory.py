499e09eac7a214f5fe75c6c84141be4046e9829d
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2017-08-21 23:07:36 +0200

    Almost 4 steps implemented

--- gameInventory.py
+++ gameInventory.py
@@ -1,14 +1,56 @@
 # This is the file where you must work. Write code in the functions, create new functions, 
 # so they work according to the specification
 
+import csv
+
+inv = {
+    'arrow': 12,
+    'gold coin': 42,
+    'rope': 1,
+    'torch': 6,
+    'dagger': 1
+}
+
+inv2 = {
+    'diamond': 2,
+    'silver coin': 3
+}
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+dragon_loot2 = ['gold coin', 'dagger', 'gold coin', 'diamond', 'ruby', 'diamond', 'diamond', 'silver coin']
+
+
 # Displays the inventory.
 def display_inventory(inventory):
-    pass
+
+    print ("Inventory: ")
+
+    for key, value in inventory.items():
+        print (value, key)
+
+    print ("Total number of items: " + str(sum(inventory.values())))
+
+print('Step 1 ------------------------------------------------')
+display_inventory(inv)
 
 
 # Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+
+    for element in added_items:
+    
+        if element in inventory:
+            inventory[element] = inventory[element] + 1
+
+        else:
+            inventory[element] = 1
+
+    return inventory
+
+print('Step 2 ------------------------------------------------')
+inv_updated = add_to_inventory(inv, dragon_loot)
+display_inventory(inv_updated)
 
 
 # Takes your inventory and displays it in a well-organized table with 
@@ -19,7 +61,59 @@ def add_to_inventory(inventory, added_items):
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+
+    ADDITIONAL_SPACES = 3
+    LEN_COUNT = 5
+    LEN_ITEM_NAME = 9
+
+    keys = list(inventory.keys())
+ 
+    longest_key = max(keys, key=len)
+
+    len_longest_key = len(longest_key)
+
+    values = list(inventory.values())
+ 
+    max_value = max(values)
+ 
+    len_max_value = len(str(max_value))
+
+    if LEN_ITEM_NAME > len_longest_key:
+        len_longest_key = LEN_ITEM_NAME
+
+    if LEN_COUNT > len_max_value:
+        len_max_value = LEN_COUNT
+
+    value_column_len = len_max_value + ADDITIONAL_SPACES
+    
+    number_of_spaces_count = value_column_len - LEN_COUNT
+
+    key_column_len = len_longest_key + ADDITIONAL_SPACES
+
+    number_of_spaces_item_name = key_column_len - LEN_ITEM_NAME
+
+    print ('Inventory:')
+
+    print (number_of_spaces_count * ' ' + 'count' + number_of_spaces_item_name * ' ' + 'item name')
+
+    print((value_column_len + key_column_len) * '-')
+
+    for key in keys:
+        value = inventory[key]
+    
+        number_of_spaces_value = value_column_len - len(str(value))
+
+        number_of_spaces_key = key_column_len - len(key)     
+
+        print(number_of_spaces_value * ' ' + str(value) + number_of_spaces_key * ' ' + key)
+ 
+    print((value_column_len + key_column_len) * '-')
+
+    print ("Total number of items: " + str(sum(inventory.values())))
+
+
+print('Step 3 ------------------------------------------------')
+print_table(inv)
 
 
 # Imports new inventory items from a file
@@ -27,7 +121,21 @@ def print_table(inventory, order=None):
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
-    pass
+
+    inventory.clear()
+    
+    with open(filename, 'r') as f_inv:
+        reader = csv.reader(f_inv)
+        list_of_items = list(reader)
+        list_of_items = list_of_items[0]
+    
+    add_to_inventory(inventory, list_of_items)
+
+    return inventory
+
+print ('Step 4 ------------------------------------------------')
+inv_from_file = import_inventory(inv, 'test_inventory.csv')
+print_table(inv_from_file)
 
 
 # Exports the inventory into a .csv file.
